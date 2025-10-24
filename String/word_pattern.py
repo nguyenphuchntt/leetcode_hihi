aae2eab622a356f53bd234ec49cfa1da8ccd8711
Nguyen Van Phuc <nguyenvanphucuet@gmail.com>
2025-10-24 23:58:43 +0700

    2025.10.24

---
+++ String/word_pattern.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        words = s.split(' ')
+
+        if len(pattern) != len(words):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for i in range(len(pattern)):
+            char = pattern[i]
+            word = words[i]
+
+            if char in char_to_word:
+                if char_to_word[char] != word:
+                    return False
+            else:
+                char_to_word[char] = word
+
+            if word in word_to_char:
+                if word_to_char[word] != char:
+                    return False
+            else:
+                word_to_char[word] = char
+
+        return True
